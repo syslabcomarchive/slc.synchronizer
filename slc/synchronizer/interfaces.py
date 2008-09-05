cb8ec2d29095dd4db567997877c0934c55d21076
Alexander Pilz <pilz@syslab.com>
2008-09-05 14:39:40 +0000

    new

---
+++ slc/synchronizer/interfaces.py
@@ -0,0 +1,19 @@
+
+class IReceiver(Interface):
+    """ Interface for a component that receives data from a remote site to add objects to this site
+    """
+    
+    def getSyncStatus(remote_uid): modificationDate | None
+        """ looks up the remote uid in the local utility to get the local uid.
+            resolves the object using the local uid
+            returns the modification date of the referenced object or None, if no such object exists
+            as well as the link of the object
+        """
+
+    def syncObject(portal_type, data={}, remote_uid=None, translation_reference_uid=None)
+        """ check if an object to the given remote_uid exists
+            if not, create one using the portal_type
+            update its data using the data mapping
+            returns a feedback message and the link of the object in question
+        """
+        
