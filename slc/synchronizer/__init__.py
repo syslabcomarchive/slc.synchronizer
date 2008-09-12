1f35f3bb21182fb90a3d97e7c328a2ca6696ed96
Alexander Pilz <pilz@syslab.com>
2008-09-12 12:47:48 +0000

    added new role and permission

--- slc/synchronizer/__init__.py
+++ slc/synchronizer/__init__.py
@@ -1,4 +1,7 @@
+from Products.CMFCore.permissions import setDefaultRoles
 GLOBALS = globals()
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
+    SynchronizeContent = 'Synchronize portal content'
+    setDefaultRoles( SynchronizeContent, ( 'Manager', 'Owner' ) )
