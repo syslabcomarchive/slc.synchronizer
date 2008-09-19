95f76abf54595262b7c4647199c3bb6cb34331e4
Alexander Pilz <pilz@syslab.com>
2008-09-19 12:41:05 +0000

    implemented credential annotation and recognition

--- slc/synchronizer/browser/synchronizer.py
+++ slc/synchronizer/browser/synchronizer.py
@@ -6,6 +6,9 @@ from Products.CMFCore.utils import getToolByName
 from slc.synchronizer.interfaces import ISynchronizer, IAccessStorage
 from xmlrpclib import ServerProxy
 from zope.component import queryUtility
+from persistent import Persistent
+from zope.annotation.interfaces import IAnnotations
+from AccessControl import Unauthorized
 
 #XXX: Should also work without Linguaplone
 try:
@@ -14,6 +17,14 @@ try:
 except:
     HAVE_LP = False    
     
+ANNOKEY = "slc.synchronizer.data"    
+    
+class SyncSettings(Persistent):
+    """ store sync status information 
+    """    
+    credentials = ''
+    
+        
 class Synchronizer(BrowserView):
     """ 
     """
@@ -28,12 +39,13 @@ class Synchronizer(BrowserView):
 
 
     def __call__(self):
+        """ handle the form input
+        """
         R = self.context.REQUEST
         if R.has_key('savecredentials') and R.get('savecredentials', '') != '':
             self._save_credentials()
             
-        if R.has_key('form.button.Synchronize'):
-            
+        if R.has_key('form.button.Synchronize'):            
             self.syncObject(self.context.portal_type, 
                             self.get_data(), 
                             remote_uid=self.context.UID(), 
@@ -42,6 +54,8 @@ class Synchronizer(BrowserView):
 
 
     def _save_credentials(self):
+        """ save entered credentials into the storage, if the checkbox is checked
+        """
         R = self.context.REQUEST
         server = R.get('server', '')
         username = R.get('username', '')
@@ -50,6 +64,7 @@ class Synchronizer(BrowserView):
         print "s, u, p", server, username, password
         storage.add(server, username, password)                
 
+
     def get_trans(self):        
         if not HAVE_LP or not ITranslatable.implementedBy(self.context):
             return ''
@@ -57,6 +72,7 @@ class Synchronizer(BrowserView):
         if can != self.context:
             return can.UID()
         return ''
+        
         
     def get_data(self):
         data = {}
@@ -68,38 +84,66 @@ class Synchronizer(BrowserView):
                 value = '[[None]]'
             data[i] = value
         return data
-        
-    def setProxy(self, proxy):
-        """ used for testing currently..."""
-        self.proxy = proxy
     
-    def _generate_target_url(self):
+    def default_credentials(self):
+        """ called by the form it checks if there is a credentials field in the request. 
+            if not, it checks if we have a default credential key as annotation on the object
+        """
+        R = self.context.REQUEST
+        if R.get('credentials', None):
+            return R['credentials']  
+        if self.syncsettings.credentials != '':
+            return self.syncsettings.credentials
+        return ''
+    
+    def _get_credentials(self):
+        """ retrieve server, username and password from the environment 
+        """
         R = self.context.REQUEST
         server = R.get('server', '')
         username = R.get('username', '')
         password = R.get('password', '')
         if not server or not username or not password:
+            storage = queryUtility(IAccessStorage)
             if R.get('credentials', '') != '':
-                storage = queryUtility(IAccessStorage)
                 login, server = R['credentials'].split('@', 1)
                 password = storage.get(server, login, '')
+            elif self.syncsettings.credentials != '':
+                login, server = self.syncsettings.credentials.split('@', 1)
+                password = storage.get(server, login, '')                
             else:
-                return ''
-
+                return '', '', ''
+        return server, login, password
+            
+            
+    def _generate_target_url(self):
+        """ generate an xmlrpc compatible url from the credentials 
+        """
+        server, login, password = self._get_credentials()  
+        if not server or not login or not password:
+            return ''
+            
         if not server.startswith('http://'):
             server = "http://"+server
         if server[-1]=='/':
             server = server[:-1]
-        targeturl = "http://%s:%s@%s/synchronize_receiver" % (username, password, server[7:])
-        print targeturl
+
+        targeturl = "http://%s:%s@%s/synchronize_receiver" % (login, password, server[7:])
+
+        #print targeturl
         return targeturl
         
             
     @property   
     def rpc(self):
+        """ connect to targeturl via xmlrpc
+        """
         targeturl = self._generate_target_url()
+        if not targeturl:
+            return None
         proxy = ServerProxy(targeturl)
         return proxy
+        
 
     def credentials(self):
         """ read existing credentials from the storage and display login and url as key
@@ -115,21 +159,28 @@ class Synchronizer(BrowserView):
         """
         return "/".join(self.context.portal_url.getPortalObject().getPhysicalPath())
 
+
     def getSyncStatus(self):
         """ return status about last synchronization """
         targeturl = self._generate_target_url()
         if not targeturl:
             return [-1, '']
-            
+                    
         try:
             syncstat = self.rpc.getSyncStatus(self.getSiteId(), self.context.UID())
+            if syncstat[1]==-1:
+                syncstat[1]=''
         except Unauthorized, uae:
             return [-1, 'Unauthorized: %s'%str(e)]
         except Exception, e:
-            return [-1, 'Other error: %s'%str(e)]
+            return [-1, 'Error: %s'%str(e)]
         else:
             return syncstat
-        
+
+    @property
+    def syncsettings(self):
+        ann = IAnnotations(self.context)
+        return ann.setdefault(ANNOKEY, SyncSettings())
 
     def syncObject(self, portal_type, data={}, remote_uid=None, translation_reference_uid=None):
         """ check if an object to the given remote_uid exists
@@ -137,5 +188,13 @@ class Synchronizer(BrowserView):
             update its data using the data mapping
             returns a feedback message and the link of the object in question
         """
-        return self.rpc.syncObject(portal_type, data, self.getSiteId(), remote_uid, translation_reference_uid)
+        syncstat = self.rpc.syncObject(portal_type, data, self.getSiteId(), remote_uid, translation_reference_uid)
+
+        # if syncing was successful, remember the used credentials on this object
+        server, login, password = self._get_credentials()
+        if server and login:
+            cred = "%s@%s" % (login, server)
+            self.syncsettings.credentials = cred
+            
+        return syncstat
 
