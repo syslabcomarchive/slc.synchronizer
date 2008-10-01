973e8b128d25496c101631e08857ccd2e8ceeaf7
Alexander Pilz <pilz@syslab.com>
2008-10-01 10:08:19 +0000

    invalidatecache is to happen on the cacnonical

--- slc/synchronizer/browser/receiver.py
+++ slc/synchronizer/browser/receiver.py
@@ -54,7 +54,7 @@ class Receiver(BrowserView):
         canonical = canonical.getObject()
         if not ob.hasTranslation(canonical.Language()):
             ob.addTranslationReference(canonical)        
-        ob.invalidateTranslationCache()        
+        canonical.invalidateTranslationCache()        
             
             
     def syncObject(self, portal_type, 
