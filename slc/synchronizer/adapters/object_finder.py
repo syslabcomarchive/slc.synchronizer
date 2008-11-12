1ce78bb555e90c17c0269f9abaccefbe0b98d3d8
Wolfgang Thomas <thomas@syslab.com>
2008-11-12 12:13:37 +0000

    butt-ugly hack: first, strip special characters out of title for the search. Then, if the's more than one result, directly compare the titles again. Oh, and it assumes the default encoding is utf-8...

--- slc/synchronizer/adapters/object_finder.py
+++ slc/synchronizer/adapters/object_finder.py
@@ -2,6 +2,9 @@ from zope.interface import implements
 from zope.app.component.hooks import getSite
 from slc.synchronizer.interfaces import IObjectFinder
 
+ILLEGAL_CHARS =['(', ')', '-', '/', '\\', '+', '[', ']', '*', '#', '$', '%',]
+
+
 class ObjectFinder(object):
     """Finds an object based on some algorithms
     """
@@ -11,8 +14,21 @@ class ObjectFinder(object):
         portal = getSite()
         pc = portal.portal_catalog
         title = data.get('title')
-        results = pc(portal_type=portal_type, Title=title, Language='all')
+        search_title = title
+        for char in ILLEGAL_CHARS:
+            search_title = search_title.replace(char, ' ')
+        results = pc(portal_type=portal_type, Title=search_title, Language='all')
         if len(results)==1:
             return results[0].getObject()
+        elif len(results)>1:
+            possible_results = list()
+            for res in results:
+              try:
+                if unicode(res.Title, 'utf-8') == title:
+                    possible_results.append(res)
+              except Exception, err:
+                print "ObjectFinder - Error in trying to compare titles: %s" %err
+            if len(possible_results)==1:
+                return possible_results[0].getObject()
         return None
         
