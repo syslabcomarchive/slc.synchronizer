35b4a925b71599e785692e304e2a19fea4fde477
Alexander Pilz <pilz@syslab.com>
2009-06-05 15:45:20 +0000

    prepare release

--- README.txt
+++ README.txt
@@ -1,4 +1,16 @@
-Introduction
-============
+.. contents::
+
+.. Note!
+   -----
+   
+   - code repository
+   - bug tracker
+   - questions/comments feedback mail
+
+
+- Code repository: https://svn.syslab.com/svn/syslabcom/slc.synchronizer
+- Questions and comments to info (at) syslab (dot) com
+- Report bugs at http://products.syslab.com/products/slc.synchronizer/issues
+
 
 
--- setup.py
+++ setup.py
@@ -1,32 +1,68 @@
-from setuptools import setup, find_packages
+# -*- coding: utf-8 -*-
+"""
+This module contains the slc.synchronizer package
+"""
 import os
+from setuptools import setup, find_packages
 
-version = '1.0'
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '1.1'
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    'Change history\n'
+    '**************\n'
+    + '\n' +
+    read('CHANGES.txt')
+    + '\n' +
+    'Detailed Documentation\n'
+    '**********************\n'
+    + '\n' +
+    read('slc', 'synchronizer', 'README.txt')
+    + '\n' +
+    'Contributors\n'
+    '************\n'
+    + '\n' +
+    read('CONTRIBUTORS.txt')
+    + '\n' 
+    )
+    
+tests_require=['zope.testing']
 
 setup(name='slc.synchronizer',
       version=version,
-      description="",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      description="A special mechanism to sync a contenttype to another server.",
+      long_description=long_description,
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='',
+      keywords='synchronize content type',
       author='Syslab.com GmbH',
       author_email='info@syslab.com',
-      url='https://svn.plone.org/svn/collective/slc.synchronizer/trunk',
+      url='https://svn.syslab.com/svn/syslabcom/slc.synchronizer',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['slc'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
+          'setuptools',    
+          # -*- Extra requirements: -*-
       ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      test_suite = 'slc.synchronizer.tests.test_docs.test_suite',
       entry_points="""
-      # -*- Entry points: -*-
+      # -*- entry_points -*- 
       """,
-      )
+      paster_plugins = ["ZopeSkel"],
+      )      
+      
