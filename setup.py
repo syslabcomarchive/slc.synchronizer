ddf05f18be02f0f706df00218c9197c8f37caf36
Wolfgang Thomas <thomas@syslab.com>
2013-05-16 15:02:46 +0000

    Back to development: 1.2.8-gfb-hardcoded

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.2.7-gfb-hardcoded'
+version = '1.2.8-gfb-hardcoded.dev0'
 
 long_description = (
     read('README.txt')
